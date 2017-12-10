d9105d6f91c5ad4e98f1d39a76ddabab071b8cf7
fbrossar25 <florian.brossard25@gmail.com>
2017-12-10 21:57:16 +0100

    plus de tests

--- BROSSARD_DJEBRI/input.c
+++ BROSSARD_DJEBRI/input.c
@@ -1,8 +1,11 @@
 int main()
 {
-    int a = 1+1-1+1*2/2;
+    int a;
+    a = 1+1-(1+1)*2/2; // = 0
+    printi(a); //0
+    printf("\n");
     int b = 5;
-    int c = a * (b - 1 + 1);
+    int c = a * (b - 1 + 1) + 1; // = 1
     //int e = -d //erreur de syntaxe ';'
     /*
     test commentaire
@@ -11,13 +14,14 @@ int main()
     //int e = -d; //erreur sémantique sur d
     //int f = 0 / 0; //division par 0
     c++;
+    printi(c); // 2
     c--;
     c += 1;
     c-=1;
 
-    printf("Hello world !\n");
+    printf("\nHello world !\n");
     printi(42);
     printf("\n"); //caractère spéciaux à ne pas effacer
-    printi(c);
+    printi(c); // 1
     return 0;
 }
